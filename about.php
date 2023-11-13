7186d4822f7179550bc3e25d5214b4e966000cad
Ggitta_Sseruwagi <87973250+ggittasseruwagi@users.noreply.github.com>
2023-11-13 10:38:00 +0200

    for the group assignment link.

---
+++ about.php
@@ -0,0 +1,180 @@
+<?php
+
+include 'components/connect.php';
+
+if(isset($_COOKIE['user_id'])){
+   $user_id = $_COOKIE['user_id'];
+}else{
+   $user_id = '';
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+<?php include 'components/user_header.php'; ?>
+
+<!-- about section starts  -->
+
+<section class="about">
+
+   <div class="row">
+
+      <div class="image">
+         <img src="images/about-img.svg" alt="">
+      </div>
+
+      <div class="content">
+         <h3>why choose us?</h3>
+         <p>Learn.io is  website dediacted to delivering aducation on different skillsets to our local communities. People of all kinds can choose to become mentors, and curate courses to teach certain skills for a set amount. This creates opportunities for people to gain income from sharing their knowledge.</p>
+         <p>Students can choose to enrol to gain knowledge. This can help to bridge a gap in the IT industry and give opportunites for people to be employable using the skills they have learned.</p>
+         <a href="courses.html" class="inline-btn">our courses</a>
+      </div>
+
+   </div>
+
+   <div class="box-container">
+
+      <div class="box">
+         <i class="fas fa-graduation-cap"></i>
+         <div>
+            <h3>+1k</h3>
+            <span>online courses</span>
+         </div>
+      </div>
+
+      <div class="box">
+         <i class="fas fa-user-graduate"></i>
+         <div>
+            <h3>+25k</h3>
+            <span>brilliants students</span>
+         </div>
+      </div>
+
+      <div class="box">
+         <i class="fas fa-chalkboard-user"></i>
+         <div>
+            <h3>+5k</h3>
+            <span>expert teachers</span>
+         </div>
+      </div>
+
+      <div class="box">
+         <i class="fas fa-briefcase"></i>
+         <div>
+            <h3>100%</h3>
+            <span>job placement</span>
+         </div>
+      </div>
+
+   </div>
+
+</section>
+
+<!-- about section ends -->
+
+<!-- reviews section starts  -->
+
+<section class="reviews">
+
+   <h1 class="heading">student's reviews</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <p>I would like to thank the creators of this website, for it has brought education closer to us. 5 stars from me, i totally recommend this to all the students.</p>
+         <div class="user">
+            <img src="images/pic-2.jpg" alt="">
+            <div>
+               <h3>Bongani Kubheka</h3>
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+               </div>
+            </div>
+         </div>
+      </div>
+
+      <div class="box">
+         <p>in all henesty I would not change much about Learn.io i have really learned alot from the tutors and how patient and presise they are with their lectures it is reallly oustanding and for the fact that i could get 24hr assistance it is really oustanding</p>
+         <div class="user">
+            <img src="images/pic-3.jpg" alt="">
+            <div>
+               <h3>Christopher Charlie </h3>
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+               </div>
+            </div>
+         </div>
+      </div>
+
+      <div class="box">
+         <p>I got real world experience, same as learning from a university or a collage. thank you very much learni!! </p>
+         <div class="user">
+            <img src="images/pic-4.jpg" alt="">
+            <div>
+               <h3>Murwa Mabuke</h3>
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+               </div>
+            </div>
+         </div>
+      </div>
+
+      <div class="box">
+         <p>I am happy with the courses! I learned great coding skills. i would recommend.
+         </p>
+         <div class="user">
+            <img src="images/pic-5.jpg" alt="">
+            <div>
+               <h3>N WORD</h3>
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+               </div>
+            </div>
+         </div>
+      </div>
+
+   </div>
+
+</section>
+
+<!-- reviews section ends -->
+
+<?php include 'components/footer.php'; ?>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+   
+</body>
+</html>
---
+++ components/footer.php
@@ -0,0 +1,5 @@
+<footer class="footer">
+
+   &copy; copyright @ <?= date('Y'); ?> by <span>GROUP12/Project_WWW</span> | all rights reserved!
+
+</footer>
